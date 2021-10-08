7ae962d25b8d82c542457b55d72825459643732a
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-08 18:47:41 +0530

    Add files via upload

---
+++ Bollywood/Love/music-list.js
@@ -0,0 +1,65 @@
+let allMusic = [
+  {
+    name: "Raataan Lambiyan",
+    artist: "Asees Kaur and Jubin Nautiyal",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Thoda Thoda Pyaar Hua Tumse",
+    artist: "Nilesh Ahuja and Stebin Ben",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tum Hi Ho",
+    artist: "Arijit Singh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Shayad",
+    artist: "Arijit Singh, Pritam",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Enna Sona",
+     artist: "Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Sawan Aaya Hai",
+     artist: "Arijit Singh , Tony Kakkar",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Soch Na Sake",
+     artist: " Arijit Singh, Tulsi Kumar, Amaal Mallik, Amaal Mallik",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Tujhe Kitna Chahne Lage ",
+     artist: "Mithoon, Amaal Mallik, Sachet-Parampara, Vishal Mishra",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Mast Magan ",
+     artist: " Arijit Singh, Chinmayi",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Main Agar Kahoon",
+     artist: " Sonu Nigam, Shreya Ghoshal",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
