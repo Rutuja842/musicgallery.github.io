9bc6c29a0189718897a5b65612131a532f156927
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-08 18:08:40 +0530

    Add files via upload

---
+++ Bollywood/Emotional/music-list.js
@@ -0,0 +1,65 @@
+let allMusic = [
+  {
+    name: "Khairiyat ",
+    artist: "Nitesh Tiwari Arijit Singh , Pritam",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Mann Bharryaa ",
+    artist: "Shershaah , B Praak Jaani",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Teri Mitti ",
+    artist: "Arko B Praak, Manoj Muntashir",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tera Yaar Hoon Main ",
+    artist: "Arijit Singh, Rochak Kohli",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Naina ",
+     artist: " Neha Kakkar ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Chale Aana",
+     artist: "Armaan Malik",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Hasi",
+     artist: " Shreya Ghoshal , Ami Mishra",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Kabira",
+     artist: " Rekha Bhardwaj and Tochi Raina",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Ae Dil Hai Mushkil  ",
+     artist: "  Arijit ,Pritam",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Mere Sohneya",
+     artist: " Parampara Thakur and Sachet Tandon",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
