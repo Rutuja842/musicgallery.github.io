3f347911fb71ad92be7e2491bf1d00a3a408dbb6
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-08 17:58:43 +0530

    Add files via upload

---
+++ Bollywood/devotional/music-list.js
@@ -0,0 +1,65 @@
+let allMusic = [
+  {
+    name: "Kun Faya Kun ",
+    artist: "A.R. Rahman, Javed Ali, Mohit Chauhan",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Woh Kisna Hai",
+    artist: " Sukhwinder Singh ,Javed Akhtar  ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tune Mujhe Bulaya Sherawaliye",
+    artist: " Mohammed Rafi, Aasha",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Shiv Tandav Stotram ",
+    artist: " Shankar Mahadevan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Namo Namo ",
+     artist: "  Amit Trivedi , Amitabh B",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Mach Gaya Shor Sari Nagri Re ",
+     artist: "Amitabh Bachchan, Parveen Babi",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Hey Ganaraya",
+     artist: "Divya kumar",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Gajanana",
+     artist: " Sukhwinder Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Bappa  ",
+     artist: " Vishal & Shekhar",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Deva Shree Ganesha ",
+     artist: " Ajay-Atul",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
