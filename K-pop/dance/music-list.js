ec6da438ac46cd114e1ec365e5011bcfdd30da6f
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-16 19:33:51 +0530

    Add files via upload

---
+++ K-pop/dance/music-list.js
@@ -0,0 +1,65 @@
+let allMusic = [
+  {
+    name: "Not Shy",
+    artist: "ITZY",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "New Face",
+    artist: "PSY",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Back Door",
+    artist: "Stray Kids",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bapsae",
+    artist: "BTS",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Cheer up",
+     artist: " Twice",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Eclipse",
+     artist: "GOT7",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "How you like that",
+     artist: " BlackPink",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "I cant stop me",
+     artist: "Twice",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Ready to love ",
+     artist: " Seventeen",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Talk to me",
+     artist: "Red Velvet",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
