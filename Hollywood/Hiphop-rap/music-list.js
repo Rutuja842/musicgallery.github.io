fa0b4c9268538fc7f39538ef1fa13aaed57b7098
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:53:54 +0530

    Add files via upload

---
+++ Hollywood/Hiphop-rap/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Family and Loyalty",
+    artist: "Gang Starr",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Friends",
+    artist: "Marshmello, Anne Marie",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Him And I ",
+    artist: "G-Eazy, Halsey",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "I want to hold your hand ",
+    artist: "The Beatles",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Love Nwantiti ",
+     artist: " Ckay ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Montero",
+     artist: "Lil Nas X",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "My heart Stereo",
+     artist: " Gym Class Heroes",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "No Idea",
+     artist: " Don Toliver",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Numb Numb Juice ",
+     artist: " ScHoolboy Q",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "SugarCrash!",
+     artist: " ElyOtto",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
