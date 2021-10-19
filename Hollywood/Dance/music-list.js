930eaa32a95e4df79abb580e66383229b6f42005
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:52:42 +0530

    Add files via upload

---
+++ Hollywood/Dance/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Billi Jean ",
+    artist: "Micheal Jackson",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Butter ",
+    artist: "BTS",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Call Me Maybe",
+    artist: "Carly Rae Jepsen",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Cheap Thrills ",
+    artist: "Sia ft. sean paul",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Don't Start now ",
+     artist: " Dua lipa ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Dynamite",
+     artist: "BTS",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Lean On",
+     artist: " Major Lazer and DJ Snake",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Levitating",
+     artist: " Dua Lipa",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Love to hate me  ",
+     artist: "  Blackpink",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Party Rock Anthem",
+     artist: " LMFAO ",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
