12c0dd0698b1c4f5b02aa5ecb14dd3729cc847e3
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:51:18 +0530

    Add files via upload

---
+++ Bollywood/Travel/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: " Tumhi Ho Bandhu ",
+    artist: " Kavita Seth ",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: " The Humma Song ",
+    artist: " A.R. Rahman, Badshah, Tanishk",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Liggi ",
+    artist: " Ritviz ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " MATARGASHTI ",
+    artist: " Mohit Chauhan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Love You Zindagi ",
+     artist: " Amit T, Kausar M",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Journey Song  ",
+     artist: " Anupam Roy",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: " Ilahi ",
+     artist: " Arijit Singh, Pritam ",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Hawayein ",
+     artist: " Arijit Singh, Pritam ",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: " Malang ",
+     artist: " Anil K, Kunal K Ved S Mohit S ",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: " Safarnama ",
+     artist: " Lucky ali",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
