58027cbbd5a00e052eeb7e2add4484d9a296c9fe
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:52:04 +0530

    Add files via upload

---
+++ Bollywood/party/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: " The Disco Song ",
+    artist: " Sunidhi Chauhan",
+   img: "music-1",
+    src: "music-1"
+  },	
+  {
+    name: " The Breakup Song",
+    artist: " Arijit,Badshah,Jonita,Nakash Pritam",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Makhna ",
+    artist: " Tanishk Bagchi, Asees Kaur ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " Kar Gayi Chull ",
+    artist: " Badshah ,Amaal Mallik, Fazilpuri, neha kakkar ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Hook Up Song ",
+     artist: " Vishal and Shekhar Neha Kakkar Kumaar",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Garmi Song ",
+     artist: " Badshah, Neha K ",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: " Chal Bombay",
+     artist: " DIVINE",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Bom Diggy Diggy ",
+     artist: " Zack Knight Jasmin Walia ",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: " SAKI SAKI ",
+     artist: " Tanishk B, Neha K, Tulsi K, B Praak, Vishal-Shekhar",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: " Sooraj Dooba Hain",
+     artist: " Arijit singh Aditi Singh Sharma",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
