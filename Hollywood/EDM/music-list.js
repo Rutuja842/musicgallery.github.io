47ab9b9bdc3a969fabb0a05d695028b166c8ea0c
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:53:26 +0530

    Add files via upload

---
+++ Hollywood/EDM/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Bad Habits",
+    artist: "Ed Sheeran",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Criminal ",
+    artist: "Britney spears",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Ignite ",
+    artist: "K-391, Alan Walker",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lily",
+    artist: "K-391, Alan Walker, Emelie Hollow",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Love is Gone",
+     artist: " Dylan Matthew , Slander ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Meant to be",
+     artist: "Arc North",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Suagar And Brownies",
+     artist: " Dharia",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Trampoline",
+     artist: " Shaed, Zayn",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Waste it on me   ",
+     artist: "  Steve Aoki ft. BTS",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Your eyes",
+     artist: " Amero",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
