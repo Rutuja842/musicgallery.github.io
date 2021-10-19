b9d1527c3c28d831345ee2abf6a6f3670bc99ad6
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:54:33 +0530

    Add files via upload

---
+++ Hollywood/R-B/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "At My Worst ",
+    artist: "Pink Sweat",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Back and Forth ",
+    artist: "Emotional Oranges",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Bad for you ",
+    artist: "Celina Sharm",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Double Take ",
+    artist: "Dhruv",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Heartbreak Anniversary ",
+     artist: " Giveon ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "I dont wanna live forever ",
+     artist: "ZAYN, Taylor Swift",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Leave the door open",
+     artist: " Bruno Mars,  Anderson Paak, Silk Sonic",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Paradise",
+     artist: " Pink Sweat",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Peaches ",
+     artist: " Justin Bieber",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Save Your Tears",
+     artist: " The Weekend",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
