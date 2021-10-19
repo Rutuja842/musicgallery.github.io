6fba62a486209a3fdee97583fdb61db9125284a5
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:55:45 +0530

    Add files via upload

---
+++ Hollywood/Rock/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Attension ",
+    artist: "Charlie Puth",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Best song ever ",
+    artist: "One Direction",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Good 4 u ",
+    artist: "Olivia Rodrigo",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Paradise",
+    artist: "Coldplay",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Rock Me ",
+     artist: " One Diection ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "She looks so perfect",
+     artist: "5SOS",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Sombody to you",
+     artist: " The Vamps",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Steal my girl",
+     artist: " One Diection",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "What makes you Beautiful  ",
+     artist: " One Diection",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Wild Heart",
+     artist: " The Vamps",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
