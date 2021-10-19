47bf9d51faf8f3b1dfcd5da324b1ba6b192d9fc5
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-19 17:54:58 +0530

    Add files via upload

---
+++ Hollywood/pop/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Arcade ",
+    artist: "Duncan Laurence",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Drivers License ",
+    artist: "Olivia Rodrigo",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Dusk Till Down ",
+    artist: "Zayn, Sia",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Heart Attack ",
+    artist: "Demi Lovato",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Holy  ",
+     artist: "Justine Bieber ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "I like me better ",
+     artist: "Lauv",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Scares to your beautiful",
+     artist: " Alessia Cara",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Senorita",
+     artist: "Shawn Mendes, Caamila Cabello",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Stay ",
+     artist: " The kid Laroi, Justine Bieber",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Who",
+     artist: " Lauv, ft.BTS",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
