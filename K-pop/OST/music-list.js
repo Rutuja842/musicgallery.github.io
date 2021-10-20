d3d0032efa8b2e7adc9acad060c7df5438225325
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:40:31 +0530

    Add files via upload

---
+++ K-pop/OST/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "I'M Missing You  ",
+    artist: "SUNJAE",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Stay with me",
+    artist: "Chanyeol, Punch",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Breaking Down ",
+    artist: "Aaiee",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Descendants of the sun-Everytime ",
+    artist: "Vs",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "For You",
+     artist: " Chen, Baekhyun, Xiumin ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "I Love you boy ",
+     artist: "Suzy",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "My Love",
+     artist: " Lee Hi",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Reset ",
+     artist: "Tiger J.K.",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Same ",
+     artist: " Sung Hoon, Song Ji Eun",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Sweet Night",
+     artist: " V",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
