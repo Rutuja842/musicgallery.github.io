a0d24b8de84181b393e2256170801ae2f33dbaaf
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:43:52 +0530

    Add files via upload

---
+++ Marathi/silent/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Funkarichi Wadle",
+    artist: "Hrishikesh Ranade, Nihira Joshi-Deshpande",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Godi Madhachi (Sapan Bhurr Zal) ",
+    artist: "Anwesha",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Golu-Polu ",
+    artist: "Rohit-Raut,Priya-Bapat",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Khari ",
+    artist: "Unknown",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Olya Sanjveli ",
+     artist: " Unknown ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Saavar Re Mana",
+     artist: "Armaan Malik",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Sar Sukhachi Shravani",
+     artist: " Unknown",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Tola Tola",
+     artist: " Unknown",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Valhav Re Nakhwa  ",
+     artist: "  Prajakta Shukre",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Manna Chimb Pavasali",
+     artist: " Unknown",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
