a4321cadbf3feeb41f9b0c45349eaeed6b8ec5d8
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:44:58 +0530

    Add files via upload

---
+++ Punjabi/Emotional/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [
+  {
+    name: " Titliaan",
+    artist: " Harrdy Sandhu 
+",
+   img: "music-1",
+    src: "music-1"
+  },	
+  {
+    name: " Yaarr Ni Milyaa",
+    artist: " Hardy Sandhu, B Praak Jaani ,Arvindr Khaira ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Yaari",
+    artist: " Nikk Ft Avneet Kaur",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " Tanhaiyan ",
+    artist: " Kamal Khan Trending Boyz Mintoo Hayer ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Sawaal ",
+     artist: " Aman Saggi Bob ,Rohit Negah ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Main Teri Ho Gayi ",
+     artist: " Millind Gaba",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: " KHAAB ",
+     artist: " AKHIL PARMISH VERMA",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Ishq Tera",
+     artist: " Guru Randhawa",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: " FILHALL ",
+     artist: " BPraak Jaani ,Arvindr Khaira, Ammy Virk",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: " Soch",
+     artist: " Hardy Sandhu",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
