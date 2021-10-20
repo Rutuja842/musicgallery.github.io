feb4d76f5480ef8b43972d0eb4b7a00cb947994e
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:40:06 +0530

    Add files via upload

---
+++ K-pop/hiphop/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Airplane pt.2 ",
+    artist: "BTS",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Black Swan ",
+    artist: "BTS",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "DNA ",
+    artist: "BTS",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Gods Menu ",
+    artist: "Stray Kids",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "IDOL",
+     artist: " BTS ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "LALISA",
+     artist: "Lisa",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Mafia in the Morning",
+     artist: " ITZY",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Mommae",
+     artist: "Jay PARK",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "SOLO ",
+     artist: " Jenny",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "UGH!",
+     artist: " BTS",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
