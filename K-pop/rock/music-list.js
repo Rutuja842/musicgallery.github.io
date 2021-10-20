1e54fb9064ce462c43a5fd77796f69f9da47126a
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:42:08 +0530

    Add files via upload

---
+++ K-pop/rock/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Best of Me ",
+    artist: "BTS",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Euphoria ",
+    artist: "Jungkook",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Cat and Dog ",
+    artist: "TXT",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Fever",
+    artist: "ENHYPEN",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "I need You ",
+     artist: " BTS ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Kill this Love",
+     artist: "Blackpink",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Loser=Lover",
+     artist: " TXT",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Okey Dokey",
+     artist: " MINO",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Save Me  ",
+     artist: "  BTS",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "So What",
+     artist: " BTS",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
