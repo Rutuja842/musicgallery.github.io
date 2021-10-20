5005baa8d9a0a601d6b02118e68e3e4c86da56a2
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:46:23 +0530

    Add files via upload

---
+++ Tollywood/devotional/music-list.js
@@ -0,0 +1,42 @@
+let allMusic = [
+  {
+    name: "Harivarasanam ",
+    artist: "Vijay Prakash",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Krishnashtakam ",
+    artist: "S.P.Balasubrahmanyam, S.Jaykumar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Krishna Nee Begane ",
+    artist: "Vijay Prakash",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Kurai Ondrum Illai ",
+    artist: "Haricharan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Maha Ganapathim Spiritual Fusion Rock ",
+     artist: " smita ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Vathapi Ganapathim Bhajeham",
+     artist: "Haricharan",
+    img: "music-6",
+    src: "music-6"
+  },
+    
+ 
+ 
+];
+
