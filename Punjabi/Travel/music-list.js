83e62a8aac3b1ed466d5cf29bc9bed07e7ce8162
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:46:02 +0530

    Add files via upload

---
+++ Punjabi/Travel/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [
+  {
+    name: " Laung Laachi ",
+    artist: " Mannat Noor Ammy Virk, Neeru Bajwa,Amberdeep",
+   img: "music-1",
+    src: "music-1"
+  },	
+  {
+    name: " Zindagi Tere Naal ",
+    artist: " Khan Saab, Pav Dharia",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " SLOWLY SLOWLY",
+    artist: " Guru Randhawa ft. Pitbull ,Bhushan Kumar",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " HORNN BLOW",
+    artist: " Hardy Sandhu",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Lahore",
+     artist: " Guru Randhawa",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Raat Di Gedi",
+     artist: " Diljit Dosanjh,Neeru Bajwa ,Jatinder Shah ",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "JOKER ",
+     artist: " Hardy Sandhu",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Ik Tera ",
+     artist: " Maninder Buttar ",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: " Kya Baat Ay ",
+     artist: " Harrdy Sandhu ,Jaani B Praak ,Arvindr Khaira ",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "8 Parche ",
+     artist: " Baani Sandhu, Gur Sidhu, Gurneet Dosanjh",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
+
