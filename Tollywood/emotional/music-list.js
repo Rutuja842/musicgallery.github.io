e4ad999a5c54e455e5249de06c24375bace71066
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:46:42 +0530

    Add files via upload

---
+++ Tollywood/emotional/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Em Chappanu ",
+    artist: "Karthik, Devi shree prasad",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Kanavae Kanavae ",
+    artist: "Anirudh Ravichandar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Karige Loga ",
+    artist: "Devi Shree Prasad",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Nee Choopule ",
+    artist: "Haricharan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Nuvve Nuvve Kavalantundi ",
+     artist: " K.S.Chitra ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Nuvvele Nuvvele ",
+     artist: "Shweta Mohan, Sharmila",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Nuvvu Nenu",
+     artist: " Usha, R.P.Patnaik",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Imaikka Nodigal ",
+     artist: " Hip hop Tamizha",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Imaye Imaye ",
+     artist: "G.V.Prakash Kumar,Shaktishree Gopalan",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Uppenantha",
+     artist: " K.K., Devi Shree Prasad",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
