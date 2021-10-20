10e60d4909a09b45f0f291908df35c1c687bb0a4
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:45:37 +0530

    Add files via upload

---
+++ Punjabi/party/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [
+  {
+    name: " BROWN MUNDE ",
+    artist: " AP DHILLON ,GURINDER GILL, SHINDA KAHLON",
+   img: "music-1",
+    src: "music-1"
+  },	
+  {
+    name: " Teri Naar",
+    artist: " Nikk Ft Avneet Kaur",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " High Rated Gabru ",
+    artist: " Guru Randhawa",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " Naah ",
+    artist: " Harrdy Sandhu , Jaani B Praak  ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Oye Hoye Hoye",
+     artist: " Jassie Gill, Simar Kaur",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Dance Like ",
+     artist: " Harrdy Sandhu,Jaani B Praak",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: " Daru Badnaam",
+     artist: " Kamal Kahlon & Param Singh",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Bolo Ta Ra Ra",
+     artist: " Daler Mehndi",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "5 Taara",
+     artist: " Diljit Dosanjh",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "3 Peg Sharry Mann",
+     artist: " Mista Baaz,Parmish Verma,Ravi Raj",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
+
