2e987d057b02d0177b5bde98758e82f91e82f5e3
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:47:22 +0530

    Add files via upload

---
+++ Tollywood/party/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Blockbuster ",
+    artist: "Shreya Ghoshal, Nakash Aziz",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Come to the Party ",
+    artist: "Vijay Prakash",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Dimaak Kharab ",
+    artist: "Saketh, Kerthana Sharma",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "EY Adra Adra Naakka Mukka ",
+    artist: "Vijay Antony",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Gulaebaghavali",
+     artist: " K.Karthi",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Iddarammayilatho",
+     artist: "Devi Shree Prasad",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Rowdy Baby",
+     artist: " Dhanush,Dhee",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Private party ",
+     artist: " M.M.Manasi, M.Vickey",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Ringa Ringa  ",
+     artist: "  Priya Hemesh",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Seeti Maar",
+     artist: "Rita, Jaspreet Jasz",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
