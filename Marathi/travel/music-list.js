d4d1088c6085e73fb14cb279a36883373ef5d77b
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:44:14 +0530

    Add files via upload

---
+++ Marathi/travel/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Yena yena ",
+    artist: "sayali pankaj",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Tula japanar ahe ",
+    artist: "ADARSH SHINDE & Ronkini Gupta",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Sairat zal ji ",
+    artist: "Ajay Gogavale, Chinmayi Sripada. ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Datale reshami ",
+    artist: "CHINAR-MAHESH , MAHALAXMI IYER",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Mitwa ",
+     artist: "Shankar Mahadevan, Janhavi Prabhu Arora  ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Swapn chalun ale",
+     artist: "Sonu Nigam, Sayali Pankaj",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Tu havishi",
+     artist: " Priyanka Barve and Sonu Nigam",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Man he pakharu",
+     artist: " Janhvi Prabhu Arora",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Mohini  ",
+     artist: " Shreya Ghoshal, Jasraj Joshi",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Saazni",
+     artist: " shekhar ravjiyani",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
