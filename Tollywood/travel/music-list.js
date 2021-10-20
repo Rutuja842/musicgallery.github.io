c6d70d6561017753ce49befbdaa4e4eb83e02714
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:48:16 +0530

    Add files via upload

---
+++ Tollywood/travel/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Dear Comrade ",
+    artist: "Vijay Sethupathi",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "En Kannu Kulla ",
+    artist: "Varun Vishwa",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Maa Manhari ",
+    artist: "Arup Reshmi",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Mersal Maacho ",
+    artist: "A.R.Rehman, Sid Sriram",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Oh My Kadavule",
+     artist: " Sid Sriram, Leon James ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Poongatre",
+     artist: "Yuvan Shankar Raja",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Rasaali ",
+     artist: " Satyaprakash, S.Tirupathi",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Thuli Thuli",
+     artist: "Yuvan Shankar Raja,Haricharan,Tanvi Shah",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "The Road  ",
+     artist: "  The Kinks",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Yelo Pullelo",
+     artist: " Anirudh Ravichander",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
