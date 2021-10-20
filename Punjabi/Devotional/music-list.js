068bcff48b0c0e148370209be12c9fc3102d953b
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:44:36 +0530

    Add files via upload

---
+++ Punjabi/Devotional/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: " Wah Guru",
+    artist: " Jarnail Singh Laddi Gill Sudh Singh ",
+   img: "music-1",
+    src: "music-1"
+  },	
+  {
+    name: " Sarbat Da Bhala",
+    artist: " R Nait ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Nanak Niva Jo Challe",
+    artist: " Bobby  ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " Aar Nanak Paar Nanak",
+    artist: " DILJIT DOSANJH  ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Nanak Aadh Jugaadh Jiyo",
+     artist: " Diljit Dosanjh, Harmanjeet Gurmoh",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Naanki Da Veer",
+     artist: " Diljit Dosanjh ,Dhan Guru Nanak Lokdhun",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: " Mere Sahib",
+     artist: " Gippy Grewal",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Tera Bhawan Hai Rang Branga",
+     artist: " Master Saleem",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: " Gobinda Mere Gobinda",
+     artist: " Bhai Harmeet Singh Ji ",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: " Ek Onkar ",
+     artist: " Guru Granth Sahib Asees Kaur",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
