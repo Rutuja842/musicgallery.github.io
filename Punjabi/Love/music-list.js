0bcb37563b3542b971568384ae9bcb5607764def
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:45:17 +0530

    Add files via upload

---
+++ Punjabi/Love/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: " SAKHIYAN ",
+    artist: " Maninder Buttar",
+   img: "music-1",
+    src: "music-1"
+  },	
+  {
+    name: " Mere Wala Sardar ",
+    artist: " Jugraj Sandhu ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Lehanga ",
+    artist: " Jass Manak ,Satti Dhillon ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " KALLA SOHNA NAI ",
+    artist: " Neha Kakkar, Anshul Garg",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " GAL KARKE ",
+     artist: " Asees Kaur , Anshul Garg",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: " Relation",
+     artist: " Simar-Kaur",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: " TOM And JERRY ",
+     artist: " Satbir Aujla, Satti Dhillon ,Divya Puri ",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: " Jatti Da Crush",
+     artist: " Kay Vee Singh ,Nisha Bhatt",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: " Lamberghini",
+     artist: " The Doorbeen Feat Ragini",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: " Laare",
+     artist: "Maninder Buttar",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
