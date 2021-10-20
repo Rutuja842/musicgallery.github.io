f58ca25c9c3b3695a7226a47009fcdf7f1bc4646
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:42:40 +0530

    Add files via upload

---
+++ Marathi/devotional/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Aigiri Nandini",
+    artist: "Rajalakshmee, Sanjay",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Deva Tujhya Navach Yed Lagal ",
+    artist: "Vidhit Patankar,Santosh Juvekar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "EKADANTAYA VAKRATUNDAYA ",
+    artist: "SHANKAR MAHADEVAN",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Govinda Re Gopala ",
+    artist: "Swapnil Bandodkar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Man Mandira ",
+     artist: "shankar mahadevan ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Mauli-Mauli",
+     artist: "Ajay gogavale",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Morya Morya",
+     artist: " Adarsh Shinde",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Nand Kishora ",
+     artist: " Sachin , Supriya Pilgaonkar",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Tuzya Vina Vaikunthacha karbhar  ",
+     artist: " Mrunmayee Shirish Dadke, Pragati Mukund Joshi, Rasika Ganu, Kasturi Wavare, Pallavi Telgaonkar",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "ya re ya",
+     artist: " Rohan pradhan",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
