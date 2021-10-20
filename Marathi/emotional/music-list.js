a3f203fbfe499125e13f0357309f28ef3ca1274d
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:43:07 +0530

    Add files via upload

---
+++ Marathi/emotional/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "baba  ",
+    artist: "priyanka chopra",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "harvali pakhare ",
+    artist: "shekhar ravjiyani",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "jeev rangala ",
+    artist: "ajay-atul,hariharan and sheraya ghoshal",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "kadhi na kaddhi ",
+    artist: "swapnil bandodkar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "khel mandala",
+     artist: "ajay-atul",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "kuthun harvun gele",
+     artist: "ketaki mategaokar",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "premat mi",
+     artist: " shubhankar and sonia mundhe",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "sunya sunya,
+     artist: " ketaki mategaokar,aadarsh shinde",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "tu astis tar",
+     artist: " sanjeev abhyankar",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "usavale dhage",
+     artist: "mangesh borgaokar,kirti killedar",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
