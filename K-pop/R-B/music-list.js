b510e48820f7fe975f985cf9cbc909e66f289eb7
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:41:42 +0530

    Add files via upload

---
+++ K-pop/R-B/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Blue and Grey ",
+    artist: "BTS",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: " Heaven ",
+    artist: "Ailee",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Home ",
+    artist: "BTS",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "If you Do ",
+    artist: "GOT7",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Louder than Bombs ",
+     artist: " BTS",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Maria ",
+     artist: "Hwasa",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Mikrokosmos",
+     artist: " BTS",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Pied piper",
+     artist: "BTS",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Spring Day  ",
+     artist: " BTS",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Blue Hour",
+     artist: " TXT",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
