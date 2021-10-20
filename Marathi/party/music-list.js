64bff5d660a892fc4a56c7734103a715cae82ce7
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:43:31 +0530

    Add files via upload

---
+++ Marathi/party/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "Aali Thumkat Naar ",
+    artist: "Aadarsh shinde",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Aapli-Yaari",
+    artist: "Aadarsh shinde,sonali sonavane",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Awaz Vadav DJ ",
+    artist: "Aanad shinde,Aadarsh shinde",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Dhuvun Taak ",
+    artist: "Ajay-atul",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Kalla",
+     artist: " Vishal Dadlani  ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "O-Sheth",
+     artist: "umesh gavali",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Udu Udu Zalaya",
+     artist: " Anand Shinde, Saleel Kulkarni ,Sameer Samant",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Zingaat",
+     artist: "Ajay-Atul ",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "bring it on  ",
+     artist: " ajay gogavale",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "lagnalu",
+     artist: "kaustubh gaikwad,janardan kandalkar",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
