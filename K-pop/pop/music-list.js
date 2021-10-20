8c0f2277f90bbfbbb9d4cf9651f80d87c465c9be
Rutuja842 <88126581+Rutuja842@users.noreply.github.com>
2021-10-20 18:40:56 +0530

    Add files via upload

---
+++ K-pop/pop/music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+  {
+    name: "After School ",
+    artist: "Weekly",
+   img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Any Song ",
+    artist: "Zico",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Celebrity ",
+    artist: "IU",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Dolphin ",
+    artist: "Oh My Girl",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Dumb Dumb",
+     artist: " Somi ",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "Love Scenario",
+     artist: "iKON",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "Make It right",
+     artist: " BTS",
+    img: "music-7",
+    src: "music-7"
+  },
+    {
+    name: "Psycho",
+     artist: " Red Velvet",
+    img: "music-8",
+    src: "music-8"
+  },
+    {
+    name: "Sorry Sorry ",
+     artist: "Super JUNIOR",
+    img: "music-9",
+    src: "music-9"
+  },
+    
+    {
+    name: "Zero 'o' clock",
+     artist: " BTS",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+ 
+];
+
